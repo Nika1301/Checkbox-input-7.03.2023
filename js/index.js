70d1d22e6d7bf635fe2f53fe82e74c7920c85f0e
Veronika Popova <freebird130188@gmail.com>
2023-03-07 20:27:53 +0100

    Added files index.html and index.js

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ console.clear();
 
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
+const checkSubmit = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -16,8 +17,20 @@ form.addEventListener("submit", (event) => {
 
   // --v-- write your code here --v--
 
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+  let tos = data["tos"];
+  console.log(tos);
+
+  if (tos !== "on") {
+    showTosError();
+    return;
+  }
+  hideTosError();
+  checkSubmit.removeAttribute("hidden");
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
-  alert("Form submitted");
+  /*alert("Form submitted");*/
 });
